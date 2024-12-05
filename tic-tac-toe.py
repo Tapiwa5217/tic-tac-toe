1bcd2d7d392adc3c8daedc5ca0b53d4f76b666ea
Tapiwa Musinga <musingatapiwa@gmail.com>
2024-12-05 17:11:40 -0600

    first commit

---
+++ tic-tac-toe.py
@@ -0,0 +1,6 @@
+B = {1,2,3,4,5,6,7,8,9} # set of positions on the board
+
+W = {{1,2,3},{4,5,6},{7,8,9},{1,5,9},{3,5,7},{1,4,7},{2,5,8},{3,6,9}} # set of winning moves
+
+Mx = {} # moves by player X. initialized to empty set
+Mo = {}  # moves by player O. initialized to empty set
