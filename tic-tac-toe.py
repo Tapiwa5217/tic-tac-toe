504d4a04185c89a1984d0892ac68ea841c390a6b
Tapiwa Musinga <musingatapiwa@gmail.com>
2024-12-06 21:21:25 -0600

    included rules of inference

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -5,18 +5,55 @@ W = {{1,2,3},{4,5,6},{7,8,9},{1,5,9},{3,5,7},{1,4,7},{2,5,8},{3,6,9}} # set of w
 Mx = {} # moves by player X. initialized to empty set
 Mo = {} # moves by player O. initialized to empty set
 
-# if Mx ∪ Mo is not equal to B, then keep playing
-# p = Mx ∪ Mo is equal to B
-# q = keep playing
-# ¬p → q  
+# if (Mx ∪ Mo) is not equal to B, then keep playing
 
-# if |Mx| is equal to |Mo|, then let player X play, otherwise let player O play.
-# Mc = current move
+# p: (Mx ∪ Mo) is equal to B
+# q: keep playing
 
-# if Mx ∪ Mo ⊆ B and Mx ∩ Mo = ∅, then the move is acceptable
-# if |Mx| + |Mo| < 3, then skip to next move
+# Rule: ¬p → q
+
+# if |Mx| is equal to |Mo|, then player X plays, otherwise player O plays.
+
+# r: ∣Mx∣ is equal to ∣Mo∣
+# s: Player X plays
+# t: Player O plays
+
+# Rules:  r → s
+#        ¬r → t
+
+# if and only if (Mx ∪ Mo ⊆ B) ∧ (Mx ∩ Mo = ∅), the move is acceptable
+
+# u: (Mx ∪ Mo ⊆ B) ∧ (Mx ∩ Mo = ∅)
+# v: The move is acceptable
+
+# Rule: u ↔ v
+
+# if |Mx| < 3, then skip to next move
 
 # if ∃w ∈ W such that w ⊆ Mx, then player X wins 
 # if ∃w ∈ W such that w ⊆ Mo, then player O wins
 
-# if Mx ∪ Mo = B and ∀w ∈ W, w ⊈ Mx and w ⊈ Mo, then the game is a draw
+# p: ∃w ∈ W such that w ⊆ Mx
+# q: ∃w ∈ W such that w ⊆ Mo
+# r: player X wins
+# s: then player O wins
+
+# According to modus ponens:
+# p
+# p → q
+# -----
+# ∴ q
+
+# According to modus ponens:
+# r
+# r → s
+# -----
+# ∴ s
+
+# if Mx ∪ Mo = B and ∀w ∈ W, w ⊈ (Mx ∪ Mo), then the game is a draw.
+
+# t: Mx ∪ Mo = B
+# u: ∀w ∈ W, w ⊆ (Mx ∪ Mo)
+# v: the game is a draw
+
+# t ∧ u → v 
